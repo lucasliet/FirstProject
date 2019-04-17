6d2b560ad27b5fd7b69d5528e9d5fdb0ab346997
Lucas <49222261+lucasliet@users.noreply.github.com>
2019-04-17 13:49:51 -0300

    Exemplos do Kata/Randori
    Código pronto da professora Machion

---
+++ Aula07/Carrinho.java
@@ -0,0 +1,61 @@
+// Classe usada para modelar um carrinho de supermercado
+
+import java.util.ArrayList; // importa a classe ArrayList
+
+public class Carrinho
+{
+    // atributos: variáveis que armazenam os dados de um objeto, após este
+    // ser instanciado.
+    private ArrayList <Produto> produtosEscolhidos;
+
+    // métodos construtores: usado para inicializar os atributos,
+    // quando um objeto cliente é criado (instanciado). Notar que o construtor
+    // faz chamada interna de dois métodos modificadores.
+    public Carrinho()
+    {
+        produtosEscolhidos = new ArrayList<>(400);
+    }
+
+    // outros métodos
+    
+    /*
+     * Este método deve ser executado uma vez para cada produto colocado 
+     * no carrinho.
+     * Para cada produto, escrever o nome (exemplo: "arroz"), o preço 
+     * unitário por kg (em reais) e a quantidade de produto (em kg).
+     */
+    
+    public void colocarProdutoNoCarrinho(String nomeProduto, double precoProduto, 
+    double quantidadeProduto)
+    {
+        Produto novoProduto = new Produto(nomeProduto, precoProduto, quantidadeProduto);
+        produtosEscolhidos.add(novoProduto);
+    }
+    /*remover um produto de uma posição e devolver o produto*/
+    public Produto removeProdutoDoCarrinho(int posicao){
+         Produto p = produtosEscolhidos.remove(posicao);
+         return p;
+    }  
+    /* 
+    * Este método percorre a Lista de Produtos, armazenada em um ArrayList. Colhe os valores dos atributos 
+    * de cada objeto da lista e os concatena numa String (saida). Calcula 
+    * também o total em reais gasto nas compras, concatenando-o na mesma 
+    * String. Depois, retorna essa String.
+    */
+    
+   
+    public String listaDeProdutos()
+    {
+        String saida = "";
+        double soma = 0.0;
+                
+        for( Produto produto: produtosEscolhidos )
+        {
+            saida += produto.gastoComProduto();
+            soma += produto.getQuantidade() * produto.getPrecoUnitario();
+        }
+        saida += "\n\nTotal a Pagar: R$ " + soma;
+        return saida;
+      
+    }
+}
