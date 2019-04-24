a145292f267440c3a7fc43000d77d49b6028f6e6
Lucas <49222261+lucasliet@users.noreply.github.com>
2019-04-24 12:53:13 -0300

    Exercício 1

--- Aula07/Exercícios/BlocoDeNotas.java
+++ Aula07/Exercícios/BlocoDeNotas.java
@@ -4,5 +4,36 @@ public class BlocoDeNotas{
    public BlocoDeNotas(){
       notas = new ArrayList<String>();
    }
+   public int buscar(String procura){
+      for(int i = 0; i < notas.size(); i++){    
+         String nota = notas.get(i);            
+         if(procura.equals(nota)){                      
+            return i;
+         }
+      }
+      return -1;
+   }
    
+   public void inserir(String nota){
+      notas.add(nota);
+      
+   }
+   
+   public boolean remover(String nota){
+      int posicao = buscar(nota);
+      if (posicao >= 0){
+         notas.remove(posicao);
+         return true;
+      }
+      else
+      return false;
+   }
+   
+   public String imprimir(){
+      String saida = "";
+      for (String nota:notas){
+         saida +="\n["+nota+"]";
+      }
+      return saida;
+   }
 }
