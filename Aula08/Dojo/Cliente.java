e852ccc8c9684c0302b15bd38d2d397ff81fb79d
Lucas <49222261+lucasliet@users.noreply.github.com>
2019-05-08 22:35:03 -0300

    Atualização + Atividade
    Atividade 14/05
    Criar uma classe Vendedor que tem como atributos um ArrayList de Cliente e um metódo para listar seus clientes

--- Aula08/Dojo/Cliente.java
+++ Aula08/Dojo/Cliente.java
@@ -8,49 +8,42 @@ public class Cliente {
    private int idCliente;
    private String nome;
    private String fone;
-
    public Cliente(){
    }
-
    public Cliente(int idCliente) {
       this.idCliente = idCliente;
    }
-
    public Cliente(int idCliente, String nome, String fone) {
       this.idCliente = idCliente;
       this.nome = nome;
       this.fone = fone;
    }
-
    public int getIdCliente() {
       return idCliente;
    }
-
    public void setIdCliente(int idCliente) {
       this.idCliente = idCliente;
    }
-
    public String getNome() {
       return nome;
    }
-
    public void setNome(String nome) {
       this.nome = nome;
    }
-
    public String getFone() {
       return fone;
    }
-
    public void setFone(String fone) {
       this.fone = fone;
    }
-
+   public String toString() {
+      return "Cliente [idCliente=" + idCliente + ", nome=" + nome + ", fone="+ fone + "]";
+   }
    public void incluir(Connection conn) {
       String sqlInsert = 
          "INSERT INTO cliente(id, nome, fone) VALUES (?, ?, ?)";
-   
-      try (PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+      
+	  try (PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
          stm.setInt(1, getIdCliente());
          stm.setString(2, getNome());
          stm.setString(3, getFone());
@@ -66,12 +59,11 @@ public class Cliente {
          }
       } 
    }
-
    public void excluir(Connection conn) {
       String sqlDelete = "DELETE FROM cliente WHERE id = ?";
-      try (PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
-         stm.setInt(1, getIdCliente());
       
+	  try (PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+         stm.setInt(1, getIdCliente());
          stm.execute();
       } 
       catch (Exception e) {
@@ -84,14 +76,13 @@ public class Cliente {
          }
       } 
    }
-	
    public void atualizarTelefone(Connection conn, String novoFone) {
-      String sqlUpdate = "UPDATE CLIENTE SET FONE = ? WHERE ID = ?";
+      String sqlUpdate = 
+      "UPDATE CLIENTE SET FONE = ? WHERE ID = ?";
     
       try (PreparedStatement stm = conn.prepareStatement(sqlUpdate);){
          stm.setString(1, novoFone);
          stm.setInt(2, getIdCliente());
-      
          stm.execute();
       } 
       catch (Exception e) {
@@ -105,20 +96,19 @@ public class Cliente {
       } 
    }
    public void carregar(Connection conn) {
-      String sqlSelect = "SELECT * FROM cliente WHERE cliente.id = ?";
+      String sqlSelect = 
+         "SELECT * FROM cliente WHERE cliente.id = ?";
    
       try (PreparedStatement stm = conn.prepareStatement(sqlSelect);){
          stm.setInt(1, getIdCliente());
          try (ResultSet rs = stm.executeQuery();){
-         /*este outro try e' necessario pois nao da' para abrir o resultset
+         /*este outro try e necessario pois nao da para abrir o resultset
           *no anterior uma vez que antes era preciso configurar o parametro
-          *via setInt; se nao fosse, poderia se fazer tudo no mesmo try
-          */
+          *via setInt; se nao fosse, poderia se fazer tudo no mesmo try */
             if (rs.next()) {
                this.setNome(rs.getString(2));
                this.setFone(rs.getString(3));
             }
-         
          } 
          catch (Exception e) {
             e.printStackTrace();
@@ -127,34 +117,43 @@ public class Cliente {
       catch (SQLException e1) {
          System.out.print(e1.getStackTrace());
       }
-   } 
-   
-   public String toString() {
-      return "Cliente [idCliente=" + idCliente + ", nome=" + nome + ", fone="	+ fone + "]";
    }
-   
-   public void listarPedidos(Connection conn){
-      String consulta = "SELECT * FROM pedido WHERE id_cliente = ?";
-      try (PreparedStatement stm = conn.prepareStatement(consulta);){
-         stm.setInt(1, getIdCliente());
-         try (ResultSet rs = stm.executeQuery();){
-            while(rs.next()){
-               Pedido p = new Pedido();
-               
-               p.setId(rs.getInt(1));
-               p.setData(rs.getDate(2));
-               p.setValor(rs.getDouble(3));
-               
-               System.out.println(p);
-            }
-         }
-         catch (Exception e1){
-            e1.printStackTrace();
-         }
-         
-      }
-         catch (Exception e2){
-            e2.printStackTrace();
-         }
-   }
+   public ArrayList<Pedido> carregarPedidos(Connection conn) {
+		String sqlSelect = "SELECT id, data, valor FROM pedido WHERE id_cliente = ?";
+		ArrayList<Pedido> lista = new ArrayList<>();
+		
+		try (PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, getIdCliente());
+			try (ResultSet rs = stm.executeQuery();) {
+				/*
+				 * este outro try e' necessario pois nao da' para abrir o
+				 * resultsetno anterior uma vez que antes era preciso configurar
+				 * o parametrovia setInt; se nao fosse, poderia se fazer tudo no
+				 * mesmo try
+				 */
+				while (rs.next()) {
+					Pedido p = new Pedido();
+					p.setId(rs.getInt("id"));
+					p.setData(rs.getDate("data"));
+					p.setValor(rs.getDouble("valor"));
+					lista.add(p);
+				}
+			} 
+         catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 
+      catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return lista;
+	}
+	public String listaDePedidos(Connection conn){
+      ArrayList<Pedido> pedidos = carregarPedidos(conn);
+		String listagem = "";
+		for(Pedido pedido: pedidos){
+			listagem += pedido.toString() + "\n";
+		}
+		return listagem;
+	}
 }
---
+++ Aula08/Dojo/Teste2.java
@@ -0,0 +1,76 @@
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Teste2 {
+
+   public static void main(String[] args) {
+      Connection conn = null;
+      Pedido p;
+      Cliente c;
+      
+      try {
+      	// obtem conexao com o banco
+         ConexaoBD bd = new ConexaoBD();
+         conn = bd.conectar();
+         
+         //formatador de data
+         DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+     	   // *** IMPORTANTE ***: Forca o uso de transacao.
+         conn.setAutoCommit(false);
+      	                  
+         c = new Cliente(1010, "Ze dos Pedidos", "1199995678");
+         c.incluir(conn);
+         
+         p = new Pedido(1, (Date)formatter.parse("02/05/2018"), 100.50);
+         p.incluir (1010, conn);
+         
+         p = new Pedido(2, (Date)formatter.parse("03/05/2018"), 758.61);
+         p.incluir (1010, conn);
+         
+         conn.commit();
+         
+         Pedido doBanco = new Pedido(1401);
+         doBanco.carregar(conn);
+         System.out.println (doBanco);
+         
+         doBanco.atualizar (conn, (Date)formatter.parse("08/05/2018"), 1000.00);
+         doBanco.carregar(conn);
+         System.out.println (doBanco);
+                           
+         conn.commit();
+         
+         Cliente novo = new Cliente(1010);
+         novo.carregar(conn);
+         System.out.println(novo);
+         System.out.println(novo.listaDePedidos(conn));
+         
+         conn.commit();
+                
+       } 
+      catch (Exception e) {
+         e.printStackTrace();
+         if (conn != null) {
+            try {
+               conn.rollback();
+            } 
+            catch (SQLException e1) {
+               System.out.print(e1.getStackTrace());
+            }
+         }
+      } 
+      finally {
+         if (conn != null) {
+            try {
+               conn.close();
+            } 
+            catch (SQLException e1) {
+               System.out.print(e1.getStackTrace());
+            }
+         }
+      }
+   }
+}
