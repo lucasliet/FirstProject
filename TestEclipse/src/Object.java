e10c6a213605f1d57e8f0ea2537601df99b8a84f
lucasliet <lucasliet@live.com>
2019-05-15 14:07:53 -0300

    Remove TesteIDEs

--- TestEclipse/src/Object.java
+++
@@ -1,6 +0,0 @@
-
-public class Object {
-	public String Heelo() {
-		return "Hello my beaultiful World :)";
-	}
-}
--- TestIntelliJ/src/Object.java
+++
@@ -1,5 +0,0 @@
-public class Object {
-    public String Heelo(){
-        return "Hello my cruel World...";
-    }
-}
